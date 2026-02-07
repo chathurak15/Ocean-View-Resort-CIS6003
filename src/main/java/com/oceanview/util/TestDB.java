f62789f0ed49b7c99d998692dd4b94fec049ebd6
chathura <chathurakavindu15@gmail.com>
2026-02-07 21:14:10 +0530

    Initialize project with basic structure, database connection utility

---
+++ src/main/java/com/oceanview/util/TestDB.java
@@ -0,0 +1,8 @@
+package com.oceanview.util;
+
+public class TestDB {
+    public static void main(String[] args) {
+        System.out.println("Testing connection...");
+        DBConnection.getInstance();
+    }
+}
