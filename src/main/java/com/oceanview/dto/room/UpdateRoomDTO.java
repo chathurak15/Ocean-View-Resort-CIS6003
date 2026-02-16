0ee3337fba608c7a219f6f1e5c7edbcd0fe703b4
chathura <chathurakavindu15@gmail.com>
2026-02-17 01:15:46 +0530

    Add DTOs, exception, and validation strategy for room updates

---
+++ src/main/java/com/oceanview/dto/room/UpdateRoomDTO.java
@@ -0,0 +1,44 @@
+package com.oceanview.dto.room;
+
+import com.oceanview.model.enums.RoomType;
+
+import java.math.BigDecimal;
+
+public class UpdateRoomDTO {
+    private String roomName;
+    private String roomDescription;
+    private BigDecimal roomPrice;
+    private RoomType roomType;
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public String getRoomDescription() {
+        return roomDescription;
+    }
+
+    public void setRoomDescription(String roomDescription) {
+        this.roomDescription = roomDescription;
+    }
+
+    public BigDecimal getRoomPrice() {
+        return roomPrice;
+    }
+
+    public void setRoomPrice(BigDecimal roomPrice) {
+        this.roomPrice = roomPrice;
+    }
+
+    public RoomType getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(RoomType roomType) {
+        this.roomType = roomType;
+    }
+}
